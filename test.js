8d70c0b184890561e727c74668a350ba87685e58
calebu <calebadeyemi@gmail.com>
2024-12-23 19:30:48 -0500

    Adding test js file and new GHA workflow to test prettier formatting check

--- test.js
+++ test.js
@@ -1,6 +1,54 @@
-type Person = { name: string; age: number; premeum?: boolean }; // Define the type
-let myObj = { name: "Riyad", age: 22 }; // Assign myObj
-const alAmin: Person = { name: "Jobayer", age: 26, premeum: true }; // Another object
-let jobayer: Person = alAmin, sadik: Person = { name: "Sadik", age: 24 }; // Two objects in one line
-const greetPerson = (personObj: Person): string => `Hello ${personObj.name}`; // Greet function
-const personIsPremium = (personObj: Person): string => `${personObj.name}${personObj.premeum ?? ""}`; // Handle possible undefined
+let myObj = {};
+myObj = anArr;
+
+myObj = {
+  name: "Riyad",
+  age: 22,
+};
+
+const alAmin: {
+  name: string;
+  age: number;
+  premeum?: boolean;
+} = {
+  name: "Jobayer",
+  age: 26,
+  premeum: true,
+};
+
+type Person = {
+  name: string;
+  age: number;
+  premeum?: boolean;
+};
+
+/*
+We also can use 'interface' instead of 'type'. Syntax: interface Person {} interface is more preferrable on object & class.
+*/
+
+let jobayer: Person = {
+  name: "Jobayer",
+  age: 26,
+  premeum: true,
+};
+
+let sadik: Person = {
+  name: "Sadik",
+  age: 24,
+};
+
+/* type/interface in Function */
+const greetPerson = (personObj: Person): string => {
+  return `Hello ${personObj.name}`;
+};
+
+const personIsPremium = (personObj: Person): string => {
+  let result: string;
+  /*
+    X - result = personObj.premeum.toString()
+    'personObj.premeum' is possibly 'undefined'
+  */
+
+  result = personObj.name + personObj.premeum;
+  return result;
+};
