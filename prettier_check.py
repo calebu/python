47cc52069f11add150c45d80c0629f4d48a4440a
calebu <calebadeyemi@gmail.com>
2024-12-27 22:44:58 -0500

    Github checks for branch protection rules

--- prettier_check.py
+++ prettier_check.py
@@ -1,7 +1,5 @@
 import sys, subprocess
 
 files =  ' '.join(sys.argv[1:]).split(' ')
-subprocess.call("docker pull ghcr.io/tmknom/dockerfiles/prettier")
 for file in files:
-    res = subprocess.call(f"docker run --rm -v $(pwd):/work tmknom/prettier --check {file} --parser typescript >> output.txt 2>&1", shell=True)
-subprocess.call("cat output.txt")
+    res = subprocess.call(f"docker pull ghcr.io/tmknom/dockerfiles/prettier && docker run --rm -v $(pwd):/work tmknom/prettier --check {file} --parser typescript >> output.txt 2>&1", shell=True)
