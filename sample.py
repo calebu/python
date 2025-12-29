800ffeb0a3c6337ed1944e6c19e9e5f542648cb6
calebu <calebadeyemi@gmail.com>
2025-12-29 18:32:32 -0500

    Testing PR auto-comment

--- sample.py
+++ sample.py
@@ -5,6 +5,9 @@
 import sys, json, logging, yaml
 
 logger = logging.getLogger(__name__)
+if len(sys.argv) > 1 and sys.argv[1] == 'check-changes':
+	print("<html><body><h3>Changes to be made</h3><table><tr><td>Repo</td><td>Branch</td><td>Change</td></tr><tr><td>Repo 1</td><td>Main</td><td>++ line added</td></tr></table></body></html>")
+	exit()
 print (len(sys.argv))
 print (f"From python code: {sys.argv[1]}")
 
@@ -13,9 +16,6 @@ with open('main/test.yml', 'r') as file:
 
 print(config)
 
-if len(sys.argv) > 1 and sys.argv[1] == 'check-changes':
-	print("<html><body><h3>Changes to be made</h3><table><tr><td>Repo</td><td>Branch</td><td>Change</td></tr><tr><td>Repo 1</td><td>Main</td><td>++ line added</td></tr></table></body></html>")
-	exit()
 f = open("tester.txt", "a")
 f.write("[]")
 f.close()
