daa31d158e7a6beb6b26a6efa9c8fb557c047327
Caleb Adeyemi <31473599+calebu@users.noreply.github.com>
2026-01-06 12:56:55 -0500

    Update check.py

--- scripts/check.py
+++ scripts/check.py
@@ -2,10 +2,6 @@ import git
 import os
 import sys
 
-# Define the repository URL and the local destination path
-repo_url = "https://github.com/gitpython-developers/QuickStartTutorialFiles.git"
-local_dir = "./cloned_repo" # The local directory to clone into
-
 comments = sys.argv[1].splitlines()
 for comment_line in comments:
   if 'action' in comment_line:
@@ -22,7 +18,7 @@ for comment_line in comments:
       repo = git.Repo.clone_from('https://github.com/calebu_/' + source[1], source[1])
       print(f"Repository successfully cloned to {os.path.abspath(source[1])}")
     except Exception as clone_exc:
-      print(f"Unable to clone source {source[1]}")
+      print(f"Unable to clone source repo: {clone_exc}")
       exit()
   elif 'target' in comment_line:
     target = comment_line.split(':')
@@ -31,5 +27,5 @@ for comment_line in comments:
       repo = git.Repo.clone_from('https://github.com/calebu_/' + target[1], target[1])
       print(f"Repository successfully cloned to {os.path.abspath(target[1])}")
     except Exception as clone_exc:
-      print(f"Unable to clone source {target[1]}")
+      print(f"Unable to clone source repo: {clone_exc}")
       exit()
