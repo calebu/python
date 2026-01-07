887126d606041f259372c3ce34aff8d56545be7f
Caleb Adeyemi <31473599+calebu@users.noreply.github.com>
2026-01-07 15:37:02 -0500

    Update check.py

--- scripts/check.py
+++ scripts/check.py
@@ -1,6 +1,26 @@
 import git
 import os
 import sys
+import requests
+
+def post_comment(comment):
+  GITHUB_TOKEN = sys.argv[2]
+  COMMENT_URL=sys.argv[3]
+
+  headers = {
+      "Authorization": f"token {GITHUB_TOKEN}",
+      "Accept": "application/vnd.github+json"
+  }
+
+  data = {
+      "body": comment
+  }
+
+  try:
+    response = requests.post(COMMENT_URL, headers=headers, json=data)
+  except Exception as e:
+    print(e)
+  
 
 comments = sys.argv[1].splitlines()
 for comment_line in comments:
@@ -11,7 +31,7 @@ for comment_line in comments:
       repo = git.Repo.clone_from(f'https://calebu:{sys.argv[2]}@github.com/calebu/' + source[1], source[1])
       print(f"Repository successfully cloned to {os.path.abspath(source[1])}")
     except Exception as clone_exc:
-      print(f"Unable to clone source repo: {clone_exc}")
+      post_comment(f"Unable to clone source repo: {clone_exc}")
   elif 'target' in comment_line:
     target = comment_line.split(':')
     target[1] = target[1].strip()
@@ -19,4 +39,4 @@ for comment_line in comments:
       repo = git.Repo.clone_from(f'https://calebu:{sys.argv[2]}@github.com/calebu/' + target[1], target[1])
       print(f"Repository successfully cloned to {os.path.abspath(target[1])}")
     except Exception as clone_exc:
-      print(f"Unable to clone source repo: {clone_exc}") 
+      post_comment(f"Unable to clone source repo: {clone_exc}")
